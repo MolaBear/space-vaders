681f56c1f9527218cd46f3b7ff01478e40dd9152
Mola Bear <mphmola1@gmail.com>
2024-02-18 18:06:11 +0200

    new counter class

--- index.js
+++ index.js
@@ -9,50 +9,50 @@ class Player {
         this.velocity = {
             x: 0,
             y: 0
-        }   
+        };   
         
-        this.rotation = 0
+        this.rotation = 0;
 
-        const image = new Image()
-        image.src = './img/spaceship.png'
+        const image = new Image();
+        image.src = './img/spaceship.png';
         image.onload = () =>{
-            const scale = .15
-            this.image = image
+            const scale = .15;
+            this.image = image;
             this.width = image.width *scale;
             this.height = image.height *scale;
             this.position = {
                 x: canvas.width/2 - this.width/2,
                 y: canvas.height - this.height*2
-            }
-        }
+            };
+        };
     }
 
     draw() {
-        c.save()
+        c.save();
         c.translate(player.position.x + player.width/2, 
                     player.position.y + player.height/2
-        )
-        c.rotate(this.rotation)
+        );
+        c.rotate(this.rotation);
 
         c.translate(
             -player.position.x - player.width/2, 
             -player.position.y - player.height/2
-        )
+        );
 
         c.drawImage(this.image, 
                     this.position.x, 
                     this.position.y, 
                     this.width, 
                     this.height
-        )
+        );
 
-        c.restore()
+        c.restore();
     }
     update(){
         if(this.image){
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
         }
     }
 }
@@ -134,6 +134,20 @@ class Invader {
     }
 }
 
+class Counter {
+    constructor() {
+        this.counter = 0;
+    }
+
+    incrementCounter() {
+        this.counter++;
+    }
+
+    drawCounter() {
+        console.log(this.counter)
+    }
+}
+
 function endGame() {
     gameRunning = false; 
     const result = confirm("Game over! Do you want to restart?");
@@ -143,11 +157,7 @@ function endGame() {
 }
 
 function restartGame() {
-   
     gameRunning = true;
-    player.position.x;
-    player.position.y;
-
     player.position = {
         x: canvas.width / 2 - player.width / 2,
         y: canvas.height - player.height * 2
@@ -160,52 +170,45 @@ function restartGame() {
         };
     });
 }
+
 const player = new Player();
 const projectiles = [];
 const invaders = [];
+const counter = new Counter();
 
 const keys = {
-    ArrowLeft:{
-        pressed:false
-    },ArrowRight:{
-        pressed:false
-    },ArrowUp:{
-        pressed:false
-    },ArrowDown:{
-        pressed:false
-    },space:{
-        pressed:false
-    }
-}
+    ArrowLeft: {pressed: false},
+    ArrowRight: {pressed: false},
+    ArrowUp: {pressed: false},
+    ArrowDown: {pressed: false},
+    space: {pressed: false}
+};
 
-let frames = 0
-let randomInterval = Math.floor(Math.random() * 500 + 500)
-
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 500 + 500);
 let gameRunning = true;
+
 function gameLoop() {
     if (!gameRunning) return; 
 
     requestAnimationFrame(gameLoop);
-    c.fillStyle = 'black'
-    c.fillRect(0,0,canvas.width, canvas.height)
+    c.fillStyle = 'black';
+    c.fillRect(0,0,canvas.width, canvas.height);
     
-
     player.update();
     
     projectiles.forEach((projectile, index) =>{
         if (projectile.position.y + projectile.radius <= 0) {
             setTimeout(() => {
-                projectiles.splice(index, 1)
-            },0)
-        }else{
-            projectile.update()
+                projectiles.splice(index, 1);
+            },0);
+        } else {
+            projectile.update();
         }
     });
 
-    invaders.forEach((invader, index) => {
-        invader.update();
+    invaders.forEach((invader, index) => { invader.update();
 
-        
         projectiles.forEach((projectile, j) => {
             if (
                 projectile.position.y - projectile.radius <=
@@ -220,29 +223,30 @@ function gameLoop() {
                 setTimeout(() => {
                     invaders.splice(index, 1);
                     projectiles.splice(j, 1);
+                    counter.incrementCounter();
+                    counter.drawCounter();
                 }, 0);
             }
         });
     });
 
-
     if(keys.ArrowLeft.pressed && player.position.x >=0){
-        player.velocity.x = -5
-        player.rotation = -0.15
-    }else if(keys.ArrowRight.pressed && player.position.x + player.width <= canvas.width){
-        player.velocity.x = 5
-        player.rotation = 0.15
-    }else{
-        player.velocity.x = 0
-        player.rotation = 0
+        player.velocity.x = -5;
+        player.rotation = -0.15;
+    } else if(keys.ArrowRight.pressed && player.position.x + player.width <= canvas.width){
+        player.velocity.x = 5;
+        player.rotation = 0.15;
+    } else{
+        player.velocity.x = 0;
+        player.rotation = 0;
     }
     
     if(keys.ArrowUp.pressed && player.position.y >=0){
-        player.velocity.y = -5
-    }else if(keys.ArrowDown.pressed && player.position.y + player.height <= canvas.height){
-        player.velocity.y = 5
-    }else{
-        player.velocity.y = 0
+        player.velocity.y = -5;
+    } else if(keys.ArrowDown.pressed && player.position.y + player.height <= canvas.height){
+        player.velocity.y = 5;
+    } else{
+        player.velocity.y = 0;
     }
     
     if (frames % randomInterval === 0) {
@@ -255,21 +259,20 @@ function gameLoop() {
 
 gameLoop();
 
-
 addEventListener('keydown', ({key}) =>{
     switch(key){
         case 'ArrowLeft':
             keys.ArrowLeft.pressed = true;
-            break
+            break;
         case 'ArrowRight':
             keys.ArrowRight.pressed = true;
-            break
+            break;
         case 'ArrowUp':
             keys.ArrowUp.pressed = true;
-            break
+            break;
         case 'ArrowDown':
             keys.ArrowDown.pressed = true;
-            break
+            break;
         case ' ':
             projectiles.push(new Projectile({
                     position:{
@@ -280,26 +283,26 @@ addEventListener('keydown', ({key}) =>{
                         x:0,
                         y:-10
                     }
-            }))
-            break
+            }));
+            break;
     }
-})
+});
 
 addEventListener('keyup', ({key}) =>{
     switch(key){
         case 'ArrowLeft':
             keys.ArrowLeft.pressed = false;
-            break
+            break;
         case 'ArrowRight':
             keys.ArrowRight.pressed = false;
-            break
+            break;
         case 'ArrowUp':
             keys.ArrowUp.pressed = false;
-            break
+            break;
         case 'ArrowDown':
             keys.ArrowDown.pressed = false;
-            break
+            break;
         case ' ':
-            break
+            break;
     }
-})
+});
