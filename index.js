2e0c407540e893ab33b4018a686231dbd9642be3
Mola Bear <mphmola1@gmail.com>
2024-02-26 19:26:25 +0200

    separating object logic

--- index.js
+++ index.js
@@ -1,83 +1,12 @@
+import {Player} from "./js/Player"
+import {Projectile} from "./js/Projectile"
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Player {
-    constructor() {
-        this.velocity = {
-            x: 0,
-            y: 0
-        };   
-        
-        this.rotation = 0;
-
-        const image = new Image();
-        image.src = './img/spaceship.png';
-        image.onload = () =>{
-            const scale = .15;
-            this.image = image;
-            this.width = image.width *scale;
-            this.height = image.height *scale;
-            this.position = {
-                x: canvas.width/2 - this.width/2,
-                y: canvas.height - this.height*2
-            };
-        };
-    }
-
-    draw() {
-        c.save();
-        c.translate(player.position.x + player.width/2, 
-                    player.position.y + player.height/2
-        );
-        c.rotate(this.rotation);
-
-        c.translate(
-            -player.position.x - player.width/2, 
-            -player.position.y - player.height/2
-        );
-
-        c.drawImage(this.image, 
-                    this.position.x, 
-                    this.position.y, 
-                    this.width, 
-                    this.height
-        );
-
-        c.restore();
-    }
-    update(){
-        if(this.image){
-        this.draw();
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        }
-    }
-}
-
-class Projectile{
-    constructor({ position, velocity }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.radius = 5;
-    }
-
-    draw() {
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        c.fillStyle = 'red';
-        c.fill();
-        c.closePath();
-    }
-
-    update() {
-        this.draw();
-        this.position.x += this.velocity.x; 
-        this.position.y += this.velocity.y;
-    }
-}
 
 class Invader {
     constructor() {
